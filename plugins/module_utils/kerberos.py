1efc4a3017c77fa8d49a07a750f3003fc3fb2d06
Romain Pignolet <romain.pignolet@dgfip.finances.gouv.fr>
2022-01-27 10:31:34 +0100

    kerberos: add Ansible modules for principal and keytab management
    - "krb_principal" to manage creation and deletion for principal
    - "krb_keytab" to manage creation and deletion for keytab and check
      if the keytab can be use to get a ticket with "kinit"
    
    Use "ktadd" instead of "xst" for creating the keytab. Can not find
    "xst" in the man page of "kadmin" inside Centos 7 man page.

--- plugins/module_utils/kerberos.py
+++ plugins/module_utils/kerberos.py
@@ -11,6 +11,17 @@ kerberos_spec = dict(
 )
 
 
+def get_kinit_cmd(kinit_bin, principal=None, keytab=None, ccache=None):
+    kinit_cmd = [kinit_bin]
+    if principal:
+        kinit_cmd.extend([principal])
+    if keytab:
+        kinit_cmd.extend(['-kt', keytab])
+    if ccache:
+        kinit_cmd.extend(['-c', ccache])
+    return kinit_cmd
+
+
 def kinit(module):
     if not module.params['kerberos']:
         return
@@ -22,18 +33,19 @@ def kinit(module):
     password = module.params['krb_password']
     ccache = module.params['krb_ccache']
 
-    kinit_cmd = [kinit_bin]
-    if principal:
-        kinit_cmd.extend([principal])
-    if keytab:
-        kinit_cmd.extend(['-kt', keytab])
-    if ccache:
-        kinit_cmd.extend(['-c', ccache])
+    kinit_cmd = get_kinit_cmd(kinit_bin, principal, keytab, ccache)
     if password and not keytab:
         module.fail_json(msg='Password authentication not supported for kinit. Use a keytab instead.')
 
     module.run_command(kinit_cmd, check_rc=True)
- 
+
+
+def get_kdestroy_cmd(kdestroy_bin, ccache=None):
+    kdestroy_cmd = [kdestroy_bin]
+    if ccache:
+        kdestroy_cmd.extend(['-c', ccache])
+    return kdestroy_cmd
+
 
 def kdestroy(module):
     if not module.params['kerberos']:
@@ -46,8 +58,6 @@ def kdestroy(module):
     password = module.params['krb_password']
     ccache = module.params['krb_ccache']
 
-    kdestroy_cmd = [kdestroy_bin]
-    if ccache:
-        kdestroy_cmd.extend(['-c', ccache])
+    kdestroy_cmd = get_kdestroy_cmd(kdestroy_bin, ccache)
 
     module.run_command(kdestroy_cmd, check_rc=True)
---
+++ plugins/modules/krb_principal.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_native
+from ansible_collections.tosit.tdp.plugins.module_utils.kerberos_admin import kerberos_admin_spec, kadmin
+
+def main():
+    argument_spec = dict(
+        principal=dict(type='str', required=True),
+        state=dict(type='str', choices=['present', 'absent'], default='present'),
+        **kerberos_admin_spec
+    )
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    principal = module.params['principal']
+    state = module.params['state']
+
+    try:
+        results = {
+            'changed': False,
+        }
+        current_state = None
+
+        rc, stdout, stderr = kadmin(module, ['-q', 'getprinc {}'.format(principal)])
+        if 'Principal does not exist' in stderr:
+            current_state = 'absent'
+        else:
+            current_state = 'present'
+
+        # Case when principal does not exist
+        if current_state == 'absent':
+            if state == 'absent':
+                return module.exit_json(**results)
+            results['changed'] = True
+            if not module.check_mode:
+                kadmin(module, ['-q', 'addprinc -randkey {}'.format(principal)])
+
+        # Case when principal exists and must be remove
+        if current_state == 'present' and state == 'absent':
+            results['changed'] = True
+            if not module.check_mode:
+                kadmin(module, ['-q', 'delprinc -force {}'.format(principal)])
+
+        module.exit_json(**results)
+
+    except Exception:
+        import traceback
+        module.fail_json(msg=to_native(traceback.format_exc()))
+
+if __name__ == '__main__':
+    main()
