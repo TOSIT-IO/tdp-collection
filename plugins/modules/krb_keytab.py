7f013b1a1ed276552781c0c1b5a4bec92479fac1
Stephan Baum <stephan.baume-consultant@dgfip.finances.gouv.fr>
2025-11-20 17:22:14 +0100

    refactor(plugins): only add principal to keytab if it doesn't exist

--- plugins/modules/krb_keytab.py
+++ plugins/modules/krb_keytab.py
@@ -5,23 +5,34 @@
 # -*- coding: utf-8 -*-
 
 # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 import os
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
-from ansible_collections.tosit.tdp.plugins.module_utils.kerberos import try_kinit
-from ansible_collections.tosit.tdp.plugins.module_utils.kerberos_admin import kerberos_admin_spec, kadmin
+from ansible_collections.tosit.tdp.plugins.module_utils.kerberos import (
+    klist_kt,
+    try_kinit,
+)
+from ansible_collections.tosit.tdp.plugins.module_utils.kerberos_admin import (
+    kerberos_admin_spec,
+    kadmin,
+)
+
 
 def main():
     argument_spec = dict(
-        kinit_bin=dict(type='path', default='kinit'),
-        kdestroy_bin=dict(type='path', default='kdestroy'),
-        principal=dict(type='list', elements='str', required=True),
-        path=dict(type='path', required=True),
-        state=dict(type='str', choices=['present', 'absent'], default='present'),
-        **kerberos_admin_spec
+        kerberos=dict(type="bool", default=False),
+        kinit_bin=dict(type="path", default="kinit"),
+        kdestroy_bin=dict(type="path", default="kdestroy"),
+        klist_bin=dict(type="path", default="klist"),
+        principal=dict(type="list", elements="str", required=True),
+        path=dict(type="path", required=True),
+        state=dict(type="str", choices=["present", "absent"], default="present"),
+        krb_ccache=dict(type="str"),
+        **kerberos_admin_spec,
     )
 
     module = AnsibleModule(
@@ -30,16 +41,16 @@ def main():
         supports_check_mode=True,
     )
 
-    kinit_bin = module.params['kinit_bin']
-    kdestroy_bin = module.params['kdestroy_bin']
-    principals = module.params['principal']
-    keytab_path = module.params['path']
-    state = module.params['state']
+    kinit_bin = module.params["kinit_bin"]
+    kdestroy_bin = module.params["kdestroy_bin"]
+    principals = module.params["principal"]
+    keytab_path = module.params["path"]
+    state = module.params["state"]
 
     try:
         results = {
-            'changed': False,
-            'diff': {},
+            "changed": False,
+            "diff": {},
         }
         current_state = None
 
@@ -47,17 +58,17 @@ def main():
             raise RuntimeError("Keytab '{}' is a directory".format(keytab_path))
 
         if os.path.exists(keytab_path):
-            current_state = 'present'
+            current_state = "present"
         else:
-            current_state = 'absent'
+            current_state = "absent"
 
-        if current_state == 'absent' and state == 'absent':
+        if current_state == "absent" and state == "absent":
             return module.exit_json(**results)
 
-        if current_state == 'present':
+        if current_state == "present":
             # Case when keytab exists and must be remove
-            if state == 'absent':
-                results['changed'] = True
+            if state == "absent":
+                results["changed"] = True
                 if not module.check_mode:
                     os.remove(keytab_path)
                 return module.exit_json(**results)
@@ -66,43 +77,62 @@ def main():
             if try_kinit(module, kinit_bin, kdestroy_bin, principals, keytab_path):
                 # Update file permissions for existing keytab if needed
                 file_args = module.load_file_common_arguments(module.params)
-                results['changed'] = module.set_fs_attributes_if_different(
-                    file_args, results['changed'], results['diff'],
+                results["changed"] = module.set_fs_attributes_if_different(
+                    file_args,
+                    results["changed"],
+                    results["diff"],
                 )
 
                 return module.exit_json(**results)
 
-        # Either the keytab does not exist or it is not valid, so it must be generated
-        results['changed'] = True
+        # Either the keytab does not exist or it is not valid, so it must be generated only if there are new principals.
+        results["changed"] = True
         if not module.check_mode:
-            principal_args = ' '.join(principals)
-            rc, stdout, stderr = kadmin(module, ['-q', 'ktadd -k {} {}'.format(keytab_path, principal_args)])
-            # rc is 0 when the principal does not exist...
-            if 'Principal' in stderr and 'does not exist' in stderr:
-                raise RuntimeError("Failed to generate keytab for principal '{}': {}".format(principals, stderr))
+            for principal in principals:
+                if not os.path.exists(keytab_path) or principal not in klist_kt(
+                    module, keytab_path
+                ):
+                    rc, stdout, stderr = kadmin(
+                        module, ["-q", "ktadd -k {} {}".format(keytab_path, principal)]
+                    )
+                    # rc is 0 when the principal does not exist...
+                    if "Principal" in stderr and "does not exist" in stderr:
+                        raise RuntimeError(
+                            "Failed to generate keytab for principal '{}': {}".format(
+                                principal, stderr
+                            )
+                        )
             # Keytab generated is not guarantee to works so it must be verified,
             # for example, deleting a principal without deleting the keytab, then create the
             # same principal will reset the kvno, generate keytab in the same keytab file,
             # the keytab file will have the old kvno which is greater than the new kvno
             if not try_kinit(module, kinit_bin, kdestroy_bin, principals, keytab_path):
-                raise RuntimeError("Keytab '{}' generated for principal '{}' is not working".format(keytab_path, principals))
+                raise RuntimeError(
+                    "Keytab '{}' generated for principal '{}' is not working".format(
+                        keytab_path, principals
+                    )
+                )
 
         file_args = module.load_file_common_arguments(module.params)
 
         # Update the diff dict only if the keytab already exists,
         # there is no need to display a permission change for a newly created file
         diff = None
-        if current_state == 'present':
-            diff = results['diff']
-        results['changed'] = module.set_fs_attributes_if_different(
-            file_args, results['changed'], diff,
+        if current_state == "present":
+            diff = results["diff"]
+        results["changed"] = module.set_fs_attributes_if_different(
+            file_args,
+            results["changed"],
+            diff,
         )
 
         module.exit_json(**results)
 
     except Exception:
         import traceback
+
         module.fail_json(msg=to_native(traceback.format_exc()))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
